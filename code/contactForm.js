0f1b9f5db4945d7e8d68d56e664f6f4ab4b62700
Tristan PETIT <tristan.petit@etu.univ-grenoble-alpes.fr>
2024-05-03 09:45:08 +0200

    modifié :         Moi.html

---
+++ code/contactForm.js
@@ -0,0 +1,29 @@
+const form_contact = document.getElementById("form-contact"), button = document.getElementById("contact"),button2 = document.getElementById("contact2")
+information = document.getElementById("infom");
+let buttonV = false;
+
+button.onclick = (e)=>{
+    e.preventDefault();
+    if(!buttonV){
+        form_contact.style.display = "flex";
+        information.style.display = "none";
+    }else{
+        form_contact.style.display = "none";
+        information.style.display = "block";
+    }
+    buttonV =! buttonV;
+}
+
+button2.onclick = (e)=>{
+    console.log(e);
+    e.preventDefault();
+    if(!buttonV){
+        form_contact.style.display = "flex";
+        information.style.display = "none";
+    }else{
+        form_contact.style.display = "none";
+        information.style.display = "block";
+    }
+    buttonV =! buttonV;
+}
+
