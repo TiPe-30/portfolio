2ae73f50943e6fa46d1fdd28693a8186e1a445fa
Tristan PETIT <tristan.petit04@gmail.com>
2024-05-04 18:12:32 +0200

    modified:   Competences.html 	modified:   code/animMot.js 	modified:   style/competences.css 	modified:   style/header-footer.css 	modified:   style/moi.css

--- code/animMot.js
+++ code/animMot.js
@@ -1,12 +1,8 @@
-/**
- * génère un lettre de [0x21 , 0x7E] donc j'usqu'a 127 Math.random()*(33+127)
- * utilise une fonction setInterval plutôt que request animation frame
-*/ 
 const sortie = document.querySelector("output");
 setInterval(()=>{
     let mot = "";
     for(let i = 0;i < 50;i++){
-        mot += String.fromCharCode(Math.ceil(Math.random()*(128)));
+        mot += String.fromCharCode(Math.floor(Math.random()*(128)));
     }
     sortie.textContent = mot;
 },1000);
