932740cbe3d7b14e1ab3bb76def6189f54e4638e
Tristan Petit <tristan.petit@etu.univ-grenoble-alpes.fr>
2024-06-03 14:43:40 +0200

    modified:   code/animMot.js

--- code/animMot.js
+++ code/animMot.js
@@ -1,8 +1,7 @@
 const sortie = document.querySelector("output");
 setInterval(()=>{
     let mot = "";
-    for(let i = 0;i < 50;i++){
+    for(let i = 0;i < 50;i++)
         mot += String.fromCharCode(Math.floor(Math.random()*(128)));
-    }
     sortie.textContent = mot;
 },1000);
