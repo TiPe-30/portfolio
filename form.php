a5bcf392dbea19c7e26478a37d20c9967eda7685
Tristan Petit <tristan.petit@etu.univ-grenoble-alpes.fr>
2024-10-21 10:49:34 +0200

    modified:   form.php 	new file:   style/form.css

--- form.php
+++ form.php
@@ -4,18 +4,37 @@ $nickName = $_GET['lname'];
 $mail_from = $_GET['aMail'];
 $message = $_GET['message'];
 
-$headers = "From: \"$nickName $name\" <$mail_from>";
-mail('tristan.petit@etu.univ-grenoble-alpes.fr','Contact portfolio',$message,$headers);
+//$headers = "From: \"$nickName $name\" <$mail_from>";
+//mail('tristan.petit@etu.univ-grenoble-alpes.fr','Contact portfolio',$message,$headers);
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verification envoie mail</title>
+    <link rel="stylesheet" href="./style/form.css">
 </head>
+
 <body>
-    
+    <main>
+        <h1>Recapitulatif informations</h1>
+        <p>Vérifiez les informations envoyées : </p>
+        <div class="check">
+            <div class="field-text">
+                <p>Nom :  <span><?= $nickName ?></span></p>
+                <hr>
+                <p>Prenom :  <span><?= $name ?></span></p>
+                <hr> 
+                <p>Mail :  <span><?= $mail_from ?></span></p>
+                <hr>
+                <p>Contenu du message <br><br><span id="area-text"><?= $message ?></span></p>
+            </div>
+            <a href="./Moi.html">Retour</a>
+        </div>
+    </main>
 </body>
+
 </html>
